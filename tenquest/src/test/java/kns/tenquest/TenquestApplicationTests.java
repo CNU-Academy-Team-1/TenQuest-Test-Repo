ee6db796fd24fac0665d9f430ac0931a36e57433
Woody_K <84436996+woody35545@users.noreply.github.com>
2023-02-10 15:39:56 +0900

    Init project files

---
+++ tenquest/src/test/java/kns/tenquest/TenquestApplicationTests.java
@@ -0,0 +1,13 @@
+package kns.tenquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
